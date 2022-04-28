b38dd2b16feb18020fbc7f1c8ad4001e7bdb6488
chuxuewen <louistru@hotmail.com>
2022-04-28 22:13:00 +0800

    update test

--- test.ts
+++ test.ts
@@ -7,11 +7,11 @@ async function test() {
 		signer: new DefaultSigner('test', '2f2b4e5d1c379570da02e53eec7f12038b504b40cc84aebcdf740dd1b1c7f34d'),
 	});
 
-	await core.keys.methods.setUnlock({pwd: '0000'}); // 设备ksm自动解锁密码
+	//await core.keys.methods.setUnlock({pwd: '0000'}); // 设备ksm自动解锁密码
 	const from = await core.keys.methods.address({part_key: 'test2'}); // 通过key获取from地址
 	//await core.user.methods.bSNGasTap({address: from})// 调用BSN水龙头
 
-	if (1) {
+	if (0) {
 		const token = '0xFE6C5CA912D5b2f4C5A7639770DE3F577AD81Ba6';
 		const tokenId = '0xb642b94f6d1e377ca3c261484dfddbb166d5fb39bf75cec058d2d92c5f850f4a';
 
@@ -34,7 +34,7 @@ async function test() {
 		console.log(tx);
 	} else {
 		const token = '0x772604BAfdF6d9485bF82cd9CEeabfB132529083';
-		const tokenId = '0x07f231131b8bf5a4fd39c8ec880021163b9a2ddf4f70c0b32e3f3a3283a1b0ee';
+		const tokenId = '0x07f231131b8bf5a4fd39c8ec880021163b9a2ddf4f70c0b32e3f3a3283a1b0ef';
 			
 		const tx = await core.tx.methods.postSync({
 			chain: 5555, // bsn chain
--- test2.ts
+++ test2.ts
@@ -9,12 +9,12 @@ async function test2 () {
 
 	const tx = await core.tx.methods.post({
 		chain: 5555, // bsn chain
-		from: '0xa7Ad9207DE1417198FCc62FbF1a16B7f35C96ABf',
+		from: '0x7Ed7cda3702cCb9938b94944541701eA5b5c970b',
 		address: '0x4428E96DB72340F24C752630B2E0ff1AE285E5f7', // 协约地址
 		method: 'mintMultiTokenTo', // 协约方法
 		args: [
 			[{
-				"owner":"0xa7Ad9207DE1417198FCc62FbF1a16B7f35C96ABf",
+				"owner":"0x7Ed7cda3702cCb9938b94944541701eA5b5c970b",
 				"tokenId":"0x07f231131b8bf5a4fd39c8ec880021163b9a2ddf4f70c0b32e3f3a3283a1b0ea",
 				"uri":"",
 				"idType":false
