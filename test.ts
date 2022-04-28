9177f6fb3f14dfb9791db540d57a14c0f7de4afa
chuxuewen <louistru@hotmail.com>
2022-04-28 22:17:55 +0800

    update test

--- test.ts
+++ test.ts
@@ -11,47 +11,26 @@ async function test() {
 	const from = await core.keys.methods.address({part_key: 'test2'}); // 通过key获取from地址
 	//await core.user.methods.bSNGasTap({address: from})// 调用BSN水龙头
 
-	if (0) {
-		const token = '0xFE6C5CA912D5b2f4C5A7639770DE3F577AD81Ba6';
-		const tokenId = '0xb642b94f6d1e377ca3c261484dfddbb166d5fb39bf75cec058d2d92c5f850f4a';
+	const token = '0xFE6C5CA912D5b2f4C5A7639770DE3F577AD81Ba6';
+	const tokenId = '0xb642b94f6d1e377ca3c261484dfddbb166d5fb39bf75cec058d2d92c5f850f4a';
 
-		// 获取资产是否存在
-		var asset = await core.nft.methods.getNFT({
-			token: token,
-			tokenId: tokenId,
-		});
+	// 获取资产是否存在
+	var asset = await core.nft.methods.getNFT({
+		token: token,
+		tokenId: tokenId,
+	});
 
-		console.log(asset);
+	console.log(asset);
 
-		const tx = await core.tx.methods.postSync({
-			chain: 5555, // bsn chain
-			from: from,
-			address: token, // 协约地址
-			method: 'mint', // 协约方法
-			args: [tokenId], // 协约参数
-		});
+	const tx = await core.tx.methods.postSync({
+		chain: 5555, // bsn chain
+		from: from,
+		address: token, // 协约地址
+		method: 'mint', // 协约方法
+		args: [tokenId], // 协约参数
+	});
 
-		console.log(tx);
-	} else {
-		const token = '0x772604BAfdF6d9485bF82cd9CEeabfB132529083';
-		const tokenId = '0x07f231131b8bf5a4fd39c8ec880021163b9a2ddf4f70c0b32e3f3a3283a1b0ef';
-			
-		const tx = await core.tx.methods.postSync({
-			chain: 5555, // bsn chain
-			from: from,
-			address: token, // 协约地址
-			method: 'mintMultiTokenTo', // 协约方法
-			args: [[{
-				owner: from,
-				tokenId: tokenId,
-				// uri: 'https://www.topklout.com/1107625428569838027',
-				uri: `https://www.topklout.com/${tokenId}`,
-				idType: false,
-			}]], // 协约参数
-		});
-
-		console.log(tx);
-	}
+	console.log(tx);
 }
 
 test();
--- test2.ts
+++ test2.ts
@@ -1,28 +1,34 @@
 
 import {make, DefaultSigner} from '.';
 
-async function test2 () {
+async function test() {
 	const {core} = await make({
 		url: 'https://mvp-test.stars-mine.com/service-api',
-		signer: new DefaultSigner('hashii', 'f090dc6f513e3b6fb1413b4a866e9bcd80621cba5c0d27744b50019ee4215102'),
+		signer: new DefaultSigner('test', '2f2b4e5d1c379570da02e53eec7f12038b504b40cc84aebcdf740dd1b1c7f34d'),
 	});
 
-	const tx = await core.tx.methods.post({
+	//await core.keys.methods.setUnlock({pwd: '0000'}); // 设备ksm自动解锁密码
+	const from = await core.keys.methods.address({part_key: 'test2'}); // 通过key获取from地址
+	//await core.user.methods.bSNGasTap({address: from})// 调用BSN水龙头
+
+	const token = '0x772604BAfdF6d9485bF82cd9CEeabfB132529083';
+	const tokenId = '0x07f231131b8bf5a4fd39c8ec880021163b9a2ddf4f70c0b32e3f3a3283a1b0ef';
+
+	const tx = await core.tx.methods.postSync({
 		chain: 5555, // bsn chain
-		from: '0x7Ed7cda3702cCb9938b94944541701eA5b5c970b',
-		address: '0x4428E96DB72340F24C752630B2E0ff1AE285E5f7', // 协约地址
+		from: from,
+		address: token, // 协约地址
 		method: 'mintMultiTokenTo', // 协约方法
-		args: [
-			[{
-				"owner":"0x7Ed7cda3702cCb9938b94944541701eA5b5c970b",
-				"tokenId":"0x07f231131b8bf5a4fd39c8ec880021163b9a2ddf4f70c0b32e3f3a3283a1b0ea",
-				"uri":"",
-				"idType":false
-			}]
-		], // 协约参数
+		args: [[{
+			owner: from,
+			tokenId: tokenId,
+			// uri: 'https://www.topklout.com/1107625428569838027',
+			uri: `https://www.topklout.com/${tokenId}`,
+			idType: false,
+		}]], // 协约参数
 	});
 
 	console.log(tx);
 }
 
-test2();
+test();
