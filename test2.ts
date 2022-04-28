e37b895aabf39cd55dc89831a0867c0ad5f8a1e9
chuxuewen <louistru@hotmail.com>
2022-04-28 22:07:32 +0800

    update test

--- test2.ts
+++ test2.ts
@@ -2,21 +2,10 @@
 import {make, DefaultSigner} from '.';
 
 async function test2 () {
-
 	const {core} = await make({
 		url: 'https://mvp-test.stars-mine.com/service-api',
 		signer: new DefaultSigner('hashii', 'f090dc6f513e3b6fb1413b4a866e9bcd80621cba5c0d27744b50019ee4215102'),
 	});
-
-	const tx1 = await core.tx.methods.postSync({
-		chain: 5555, // bsn chain
-		from: '0xa7Ad9207DE1417198FCc62FbF1a16B7f35C96ABf',
-		address: '0xFE6C5CA912D5b2f4C5A7639770DE3F577AD81Ba6', // 协约地址
-		method: 'mint', // 协约方法
-		args: ['0xb642b94f6d1e377ca3c261484dfddbb166d5fb39bf75cec058d2d92c5f850f4e'], // 协约参数
-	});
-
-	console.log(tx1);
 
 	const tx = await core.tx.methods.post({
 		chain: 5555, // bsn chain
@@ -26,8 +15,9 @@ async function test2 () {
 		args: [
 			[{
 				"owner":"0xa7Ad9207DE1417198FCc62FbF1a16B7f35C96ABf",
-				"tokenId":"0x07f231131b8bf5a4fd39c8ec880021163b9a2ddf4f70c0b32e3f3a3283a1b0e9",
-				"uri":"","idType":false
+				"tokenId":"0x07f231131b8bf5a4fd39c8ec880021163b9a2ddf4f70c0b32e3f3a3283a1b0ea",
+				"uri":"",
+				"idType":false
 			}]
 		], // 协约参数
 	});
