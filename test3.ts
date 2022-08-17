b6605ec88d1259d0215b90d78896e04ce7f0c9b2
chuxuewen <louistru@hotmail.com>
2022-08-17 15:12:54 +0800

    update test

--- test3.ts
+++ test3.ts
@@ -1,7 +1,7 @@
 
 import sdk, {initialize} from './sdk';
 
-const DAOName = 'TESE_DAO2';
+const DAOName = 'TESE_DAO7';
 
 async function testMakeDAO() {
 	await initialize('https://smart-dao-rel.stars-mine.com/service-api');
@@ -24,6 +24,21 @@ async function testMakeDAO() {
 		description: DAOName,
 		operator: '0x45d9dB730bac2A2515f107A3c75295E3504faFF7',
 		chain: 4, // ChainType.RINKEBY
+		assetIssuanceTax: 100,
+		assetCirculationTax: 100,
+		defaultVoteRate: 5001,
+		defaultVotePassRate: 5001,
+		defaultVoteTime: 0,
+		memberBaseName: 'Test',
+		memberTotalLimit: 0,
+		members: [{
+			id: '1',
+			owner: '0x45d9dB730bac2A2515f107A3c75295E3504faFF7',
+			votes: 1,
+			name: 'Test',
+			description: 'Test',
+			avatar: '',
+		}],
 	});
 
 	console.log(task);
